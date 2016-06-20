ad55b9f6e9497e7ecdaa4e5563900440eab6bf0b
balesh2 <balesh@onid.oregonstate.edu>
2016-06-20 17:53:30 -0400

    tests that all input files are readable

--- test_script.sh
+++ test_script.sh
@@ -5,9 +5,12 @@ stype=$2 # gets the second cmd line arg as tentative list of start types
 compalg=$3 # gets the third cmd line arg as tentative path to compression alg.
 etypes=$4 # gets the fourth cmd line arg as tentative list of end types
 
+# temporarily overwrite the input until string parsing to a list is implemented
+etypes=("j2k" "j2c" "jp2")
+
 # if the first arg is not a number between 9 and F then error
 # or if there are more or less than 4 command line args, then error
-if [[ ("$itnum" -lt 0) || ("$itnum" -gt 9) || $# -gt 4 ]]; then
+if [[ ("$itnum" -lt 0) || ("$itnum" -gt 9) || $# -gt 4  || $# -lt 3 ]]; then
   # print usage error redirected to the error
   echo "Usage: test_script.sh <iteration number 0-9> [<start types>] \"<path to compression algorithm>\" [<end types>]"
   # exit with status of 1
@@ -42,3 +45,14 @@ fi
 #  # exit with status of 1
 #  exit 1
 #fi
+
+# if all the files in the input path are not readable or there are no files in
+# the directory, error
+for filename in ./img/in/it-$itnum/$stype/*; do
+  if [[ !(-r "$filename") ]]; then
+    # print error
+    echo "Error: not all input files in path ./img/in/it-$itnum/$stype/ are readable"
+    # exit with status of 1
+    exit 1
+  fi
+done
